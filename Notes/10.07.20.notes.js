2452da190cf55aade3ba125577d64868afb18381
Daja Mitchner <daja.mitchner@yahoo.com>
2020-10-10 13:57:59 -0400

    saturday notes

---
+++ Notes/10.07.20.notes.js
@@ -0,0 +1,9 @@
+var daja = "hi daja";
+console.log(daja);
+let javascript = 100
+console.log(javascript)
+
+//strings: a set of characters that is used to represent text and is surrounded by quotes, '',"",``
+// single quote; ''
+//double quote; ""
+//back ticks; ``
---
+++ Notes/10.10.20.notes.js
@@ -0,0 +1,38 @@
+//let daja = "d/'aja"
+//console.log(daja)
+//console.log("/n")
+//let mitchner = mitchner
+//console.log(mitchner)
+
+//let mitchner = "d/'aja mitchner"
+//console.log(mitchner)
+//console.log("/n")
+
+//let daja = "d\'aja\nmitchner"
+//console.log(daja)
+//console.log("\n")
+
+//let myName = "D\'aja Mitchner"
+//console.log(`${myName[0]}${myName[6]}`);
+
+//let myName = "D\'Aja\nMitchner"
+//console.log(myName)
+
+//let myName = "D\'aja Mitchner"
+//let firstLetter = myName[0]
+//let secondLetter = myName[6]
+//console.log(firstLetter + secondLetter)
+
+
+//let myName = "D\'Aja Mitchner"
+//console.log(myName[myName.length - 1])
+//console.log(myName.length)
+//console.log(myName[myName.length - 3])
+//console.log(myName.charAt(10))
+//console.log(myName.charAt(9))
+//console.log(myName.toUpperCase())
+//console.log(myName.toLowerCase())
+//console.log(myName.replace("Mitchner","Nicole"))
+
+let name = "daja"
+console.log(name.replace(/a/g,'-'))
