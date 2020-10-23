281830a61779774ff40c6b58490edde28f12d200
Daja Mitchner <daja.mitchner@yahoo.com>
2020-10-23 12:50:59 -0400

    saturday hw

---
+++ Notes/10.24.20.hw.js
@@ -0,0 +1,20 @@
+//let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+//console.log(days)
+//console.log(days.length) //7
+//console.log(days[4]) //'Thursday'
+//console.log(days.shift()) //'Sunday'
+//console.log(days) //[ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ]
+//console.log(days.unshift('Sunday')) //'Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
+//console.log(days.pop()) //'Saturday'
+//days.push('Saturday')
+//console.log(days)
+//days.splice(-3, 1, ('Friday Junior'))
+//conso)le.log(days)
+//console.log(days.shift())
+//let favorite = 'sunday'
+//console.log('My favorite day of the week is ' + favorite)
+// let phone = ['iphone', 'android']
+// let laptop = ['MacBook', 'HP', 'Dell']
+// console.log(phone.concat(laptop))
+
+
