b1f3aded6086c065275c088b8af5d7c35f560ffc
Daja Mitchner <daja.mitchner@yahoo.com>
2020-10-28 14:24:22 -0400

    wed hw

--- Notes/10.26.hw.js
+++ Notes/10.26.hw.js
@@ -7,39 +7,42 @@
 // function adding (num1, num2){
 //     return num1+num2
 // }
-// console.log(adding(4, 10))
+// console.log(adding(4, 10)) //14
 //14
 
-// function type (400){
-//     return ('400')
-//}
+// function namesRandom(names){
+//   return typeof names
+// }
+// console.log(namesRandom('jasmine')) //string
+// console.log(namesRandom(19)) //number
+// console.log(namesRandom(true)) //boolean
 
-// let fruits = ['apple', 'orange', 'peach', 'lemon', 'plum', 'grape', 'watermelon'];
-//  function fourthFruit(fruitArray){
-//     return 'the fourth fruit is ' + fruitArray[4]
-//  }
-// console.log(fourthFruit(fruits))//the fourth fruit is plum
+//let fruits = ['apple', 'orange', 'peach', 'lemon', 'plum', 'grape', 'watermelon'];
+//function fourthFruit(fruitArray){
+    //return 'the fourth fruit is ' + fruitArray[4]
+  //}
+//console.log(fourthFruit(fruits))//the fourth fruit is plum
 // function removeLastElement (watermelon){
 // watermelon.pop()
 //    return watermelon
 // }
 // console.log(removeLastElement(fruits))
 
-// function favFruits (fruits){
-//     return fruits
-// }
-// //console.log(fruits.slice(2,6))
+//function favFruits (fruits){
+  //return fruits.slice(2, 6)
+//}
+//console.log(favFruits(fruits))
 
-//  //function favFruits (fruits){
-//    return fruits
-// //}
-//  //fruits.push('bananas')
-// //console.log(fruits)
+//function favFruits (fruits){
+  //  return fruits
+//}
+//fruits.push('bananas')
+//console.log(fruits)
 
-// //console.log(fruits.join(' and '))
+//console.log(fruits.join(' and '))
 
 let names = ['Jasmine', 'Maurice', 'Chelsea', 'Anitria', 'Tauseef','Daja','Eddie']
-//console.log(names)
+console.log(names)
 function welcome (names){
     return `hello ${names}`
 }
@@ -50,17 +53,17 @@ function welcome (names){
 }
 console.log(welcome('Jasmine and Daja!'))
 
-function welcome (names){
-    return `Howdy ${names}`
-}
-console.log(welcome('Eddy!'))
+//function welcome (names){
+//return `Howdy ${names}`
+ //}
+//console.log(welcome('Eddy!'))
 
-function welcome (names){
-    return `Happy Monday, ${names}`
-}
-console.log(welcome('Anitria!'))
+// function welcome (names){
+//     return `Happy Monday, ${names}`
+// }
+// console.log(welcome('Anitria!'))
 
-function welcome (names){
-    return `Hey ${names}`
-}
-console.log(welcome('Tauseef!'))
+// function welcome (names){
+//     return `Hey ${names}`
+// }
+// console.log(welcome('Tauseef!'))
---
+++ Notes/10.28.hw.js
@@ -0,0 +1,87 @@
+//function sumNum (num1, num2){
+ //   return num1 + num2
+//}
+//console.log(sumNum(5, 5))//10
+
+//PROBLEM 2
+// function divisionRemainder (num1, num2){
+//     return num1 % num2
+// }
+// console.log(divisionRemainder(10, 2))
+// let number = '5'
+// let remainder = '0'
+// console.log(remainder)
+
+// function divisionRemainder2 (num1, num2){
+//     return `${num1} divided by ${num2} is ${number} with a remainder of ${remainder}`}
+//     console.log(divisionRemainder2(10, 2, 5, 0))
+//10 divided by 2 is 5 with a remainder of 0
+
+//PROBLEM 3
+// function randomNums (num1, num2){
+//     return (num1, num2)
+// }
+// let num1 = 1
+// let num2 = 1000
+// console.log(randomNums(Math.random()))//getting undefined
+
+//PROBLEM 4
+//function theType (dataType){
+  //  return typeof (dataType)
+//}
+//console.log(theType('hey')) //string
+
+//PROBLEM 5
+// function isItArray (arrayChecker){
+//     return typeof (arrayChecker)
+// }
+// console.log(isItArray) does this need array inside? getting back undefined
+
+//PROBLEM 6
+// let string = 'blue, yellow, brown, purple, green, pink'
+// console.log(string)
+// function fifthIndex (string){
+//     return (string)
+// }
+//console.log(fifthIndex(string[5])) //returning blank space
+
+//PROBLEM 7
+// function myInitials (firstName, lastName){
+//     return (firstName, lastName)
+// }
+// let firstName = 'daja'
+// let lastName = 'mitchner'
+// console.log(firstName[0]+lastName[0]) //dm
+
+//PROBLEM 8
+// let string = 'twitter, instagram, linkedin, facebook'
+// console.log(string)
+// function upperCase (string){
+//     return (string)
+// }
+// console.log(upperCase(string))//still lower case
+
+//PROBLEM 9
+// let string = ('orange', 'banana', 'apple', 'grape', 'mango')
+// function lastChar (string){
+//     return (string)
+// }
+// console.log(lastChar(string)) //mango
+
+//PROBLEM 10
+// let counties = ['Mexico','Canada','Italy','United Kingsdom', 'Thailand', 'India']
+// console.log(counties.indexOf('India'))
+// let last = 'India'
+// function lastElement (last){
+//     return `the last element in the array is ${last}`
+// }
+// console.log(lastElement(last)) //the last element in the array is India
+
+//PROBLEM 11 
+// let countries = ['Mexico','Canada','Italy','United Kingsdom', 'Thailand', 'India']
+// countries.unshift('Switzerland')
+// console.log(countries)
+// function favoriteCountry (fav){
+//     return `My favorite country is ${fav}`
+// }
+// console.log(favoriteCountry(countries[0])) //My favorite country is Switzerland
