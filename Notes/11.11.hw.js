d8d53b012f4f12803dfbcd03197d47b187269139
Daja Mitchner <daja.mitchner@yahoo.com>
2020-11-14 16:44:02 -0500

    codewars

--- Notes/11.11.hw.js
+++ Notes/11.11.hw.js
@@ -1,37 +1,50 @@
 //let numArray = [9, 12, 3, 0, 8, 50, 101, 8, 4, 15, 3, 2]
-
 //PROBLEM #1
+
 // function oddNumbers(num1){
-//     for (let i = 0; i <= num1; i++){
-//         if (i % 2 === 1){
-//             console.log[i]
+//     let odds = []
+//     for (let i = 0; i < num1.length; i++){
+//         if (num1[i] % 2 === 1){
+//            odds.push(num1[i])
 //         }
-//     } return 'done'
+//     } return odds
 // }
 // console.log(oddNumbers(numArray))
 
 //PROBLEM #2
-// function evenNums(oddNumbers){
-//     for (let i = 0; i <= numArray.length -1; i++){
-//     console.log(oddNumbers[i])
-//     } return 'done'
+// function evenNums (array){
+// let count = 0
+//     for (let i = 0; i < array.length; i++){
+//     if (array[i] % 2 === 0){
+//        count+=1
+//     }
+//     } return count
 // }
-// console.log(evenNums(numArray)) getting entire string
+// console.log(evenNums(numArray)) //7 
 
 //PROBLEM #3
 // function everyNum (num){
 //     let total = 0
 //     for (let i = 0; i < num.length; i++){
-//         if (num[i] % 2 === 1){
+        
 //             total += num[i]
 //         }
-//     } return total
+//      return total
 // }
-// console.log(everyNum(numArray)) //131
+// console.log(everyNum(numArray)) //215
 
 //PROBLEM #4
 
-
+// function acceptArray (array, value){
+//    for (let i = 0; i < array.length; i++){
+//        if (array[i] === value){
+//            return true
+//        }
+//    } 
+//    return false
+// }
+// console.log(acceptArray([40,7,8,23,43], 23)) //true
+// console.log(acceptArray(['kale', 'spinach','arugula'], 'iceberg')) //false
 
 
 
@@ -39,23 +52,37 @@
 //PROBLEM #5
 // let colors = ['blue', 'purple', 'pink']
 // function colorArray (favorite){
+//     let newArray = []
 //     for (let i = colors.length - 1; i >= 0; i--){
-//         console.log(favorite[i])
-//     } return 'done'
+//         newArray.push(favorite[i])
+//     } return newArray
 
 // }
-// console.log(colorArray(colors)) //pink purple blue done
+//console.log(colorArray(colors)) //pink purple blue done
 
 //PROBLEM #6
 // let numArray = ['1','2','3','4','5','6','7','8','9','10']
-// function notDivThree(divArr){
-//     let total = 0
-//     for (let i = 0; i < notDivThree.length; i++){
-//         if (divArr % 3 === 1){
-//             continue
-//         } total += divArr[i]
-//     } return total
+// function arrayNotByThree (param){
+//     let total = []
+//     for (let i = 0; i < param.length; i++){
+//         if (param[i] % 3 > 0){
+//         total.push(param[i])
+//     }
+//     } 
+//     return total
 // }
-// console.log(notDivThree(numArray)) //01
+// console.log(arrayNotByThree(numArray)) //1,2,4,5,7,8,10
 
 //PROBLEM #7
+
+//let numArray = ['1','2','3','4','5','6','7','8','9','10']
+// function arrayNotByThree (param){
+//     let total = 0
+//     for (let i = 0; i < param.length; i++){
+//         if (param[i] % 3 > 0){
+//         total += param[i]
+//     }
+//     } 
+//     return total
+// }
+// console.log(arrayNotByThree(numArray)) //173
---
+++ Notes/11.14.hw.js
@@ -0,0 +1,68 @@
+//https://www.codewars.com/kata/515e271a311df0350d00000f/solutions/javascript
+
+//function squareSum(numbers){
+//     let total = 0;
+//     for (let i = 0; i < numbers.length; i++){
+//         total += Math.pow(numbers[i], 2)
+//     }
+//     return total
+// }
+
+ //https://www.codewars.com/kata/568dcc3c7f12767a62000038/solutions/javascript
+
+ // function setAlarm(employed, vacation){
+// if (employed === true && vacation === false){
+//     return true
+// } else {
+//     return false
+// }
+// }
+
+//https://www.codewars.com/kata/5a2be17aee1aaefe2a000151/solutions/javascript
+
+// function arrayPlusArray (arr1, arr2){
+//     let arr3 = arr1.concat(arr2);
+//     console.log(arr3)
+//     let total = 0
+//     for (let i = 0; i <= arr3.length - 1; i++){
+//         total += arr3[i]
+//     } return total
+// }
+//https://www.codewars.com/kata/55a70521798b14d4750000a4/solutions/javascript
+// function greet(name){
+//  return `Hello, ${name} how are you doing today?`
+// }
+// console.log(greet('Ryan'))
+// console.log(greet('Shingles'))
+
+//https://www.codewars.com/kata/571d42206414b103dc0006a1/solutions/javascript
+// function arr(n){
+//     var newArr = [];
+//     for(var i = 0; i < n; i++){
+//       newArr.push(i);
+//     }
+//     return newArr;
+//   }
+
+//https://www.codewars.com/kata/57d814e4950d8489720008db/solutions/javascript
+
+//function index(array, n){
+    //  console.log(array, 'ARRAY')
+    //   console.log(n,'n')
+    //   console.log(array[n])
+      //if (array[n] === undefined){
+        //return -1
+      //}
+      //return Math.pow(array[n], n)
+    //}
+
+    https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/solutions/javascript
+
+    // function past(h, m, s){
+    //     let sec = s*1000
+    //     let minutes = m*60000
+    //     let hour = h*3600000
+    //     return hour + minutes + sec
+      
+    //   }
+
