e34de66fd863ca3582cc49b2741e6b2ac92f3ed8
Daja Mitchner <daja.mitchner@yahoo.com>
2020-12-02 11:01:55 -0500

    wed

---
+++ Notes/11.30.hw.js
@@ -0,0 +1,42 @@
+//You need to design a recursive function called replicate which will receive arguments times and number.
+// function replicate(times, number) {
+//     let result =[]
+  
+//     if (times <= 0){
+//       return [];
+//     }
+//     if (times > 0){
+//       result.push(number)
+//       times--
+//       console.log(times)
+//       result = result.concat(replicate(times, number))
+//   }
+//       return result
+//     }
+
+
+//You are guaranteed an integer argument. For any values outside the positive range, return null, nil or None .
+// function factorial (n) {
+//     let total = 1
+//    if (n < 0){
+//     return null
+//    }
+    
+//    if (n === 0){
+//   return 1;
+//     } else {
+//     return n * factorial(n-1)
+//   }
+//   return total 
+//   }
+
+//Impliment the reverse function, which takes in input n and reverses it. For instance, reverse(123) should return 321. You should do this without converting the inputted number into a string.
+// function reverse(n){
+//     let reverseNum = 0 
+//     {
+//     n = n + reverseNum
+//     return n.split().reverse().join()
+//       }
+//   }
+
+
